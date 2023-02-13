74be9d613385a8bdd6e75c5a8cee6f94c7b2932a
Magdalena Lanza <magdalan1993@gmail.com>
2023-02-13 18:53:22 -0300

    confirmacion

--- AppCoder/urls.py
+++ AppCoder/urls.py
@@ -2,9 +2,9 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('', inicio),
-    path('cursos/', cursos),
-    path('profesores/', profesores),
-    path('estudiantes/', estudiantes),
-    path('entregables/', entregables)
+    path('', inicio, name='inicio'),
+    path('cursos/', cursos, name='cursos'),
+    path('profesores/', profesores, name='profesores'),
+    path('estudiantes/', estudiantes, name='estudiantes'),
+    path('entregables/', entregables, name='entregables')
 ]
