ae80fc17a9e37f007a5dc028e7cdd0f4cddbac9f
Magdalena Lanza <magdalan1993@gmail.com>
2023-02-20 16:09:15 -0300

    actualizacion

--- AppCoder/urls.py
+++ AppCoder/urls.py
@@ -6,5 +6,7 @@ urlpatterns = [
     path('cursos/', cursos, name='cursos'),
     path('profesores/', profesores, name='profesores'),
     path('estudiantes/', estudiantes, name='estudiantes'),
-    path('entregables/', entregables, name='entregables')
+    path('entregables/', entregables, name='entregables'),
+    path('curso-formulario', curso_formulario, name='curso-formulario'),
+    path('profesor-formulario/', profesor_formulario, name='profesor-formulario')
 ]
--- AppCoder/views.py
+++ AppCoder/views.py
@@ -1,4 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import CursoFormulario, ProfesorFormulario
+from .models import Curso, Profesor
+
 #from AppCoder.models import Curso
 
 # Create your views here.
@@ -18,5 +21,40 @@ def estudiantes(request):
 
 def entregables(request):
     return render(request, 'AppCoder/entregables.html')
+
+def curso_formulario(request):
+
+    if request.method == 'POST':
+        mi_formulario = CursoFormulario(request.POST)
+
+        if mi_formulario.is_valid():
+            informacion = mi_formulario.cleaned_data
+            nuevo_curso = Curso(nombre = informacion['nombre'], camada = informacion['camada'])
+            nuevo_curso.save()
+            return redirect('inicio')
+
+        
+    mi_formulario = CursoFormulario()
+    return render(request, 'AppCoder/curso-formulario.html', {'formulario_curso': mi_formulario})
+
+
+def profesor_formulario(request):
+
+    if request.method == 'POST':
+        mi_formulario = ProfesorFormulario(request.POST)
+
+        if mi_formulario.is_valid():
+            informacion = mi_formulario.cleaned_data
+            profesor = Profesor(nombre = informacion['nombre'], apellido = informacion['apellido'], email = informacion['email'], profesion = informacion['profesion'])
+            profesor.save()
+            return redirect('inicio')
+
+        
+    mi_formulario = ProfesorFormulario()
+    return render(request, 'AppCoder/profesor-formulario.html', {'formulario_profesor': mi_formulario})
+
+
+
+
 
 
